0b3789e48b7f53bc1ec41f7743a4fd19d97154dd
andriannus <andriansimamora@gmail.com>
2019-01-08 23:05:37 +0700

    add scroll behavior

--- client/src/router.ts
+++ client/src/router.ts
@@ -127,4 +127,11 @@ export default new Router({
       ],
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
 });
